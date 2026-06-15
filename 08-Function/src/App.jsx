1caad8167b6bfe5dbaffbfee0ccdf5ddde4ccc9d
Rishikesh pal <rishikeshdrop615@gmail.com>
2026-06-15 07:34:54 +0530

    08-Function

---
+++ 08-Function/src/App.jsx
@@ -0,0 +1,18 @@
+  
+ 
+ const App = () => {
+
+ 
+   return (
+     <div>
+        <h1>Hello guy's </h1>
+
+        <button onClick={ () => {
+            console.log('btn was clicked ')
+        }}>Change user</button>
+     </div>
+   )
+ }
+ 
+ export default App
+ 
